0d3302642f73e9de66760bbb57197232f9dcb231
vida-khalili <98278710+vida-khalili@users.noreply.github.com>
2023-08-19 11:01:19 +0330

    responsive for max-width 500px

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import bgImage from "../../assets/background.svg";
+import bgMobileImage from "../../assets/background-mobile.svg";
 import "./Layout.css";
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <img src={bgImage} alt="background" />
+      <img src={bgImage} alt="background" className="background" />
+      <img src={bgMobileImage} alt="background" className="mobile-background" />
       <Header />
       {children}
       <Footer />
